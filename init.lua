ad0111b7351e7cdfcfe39c5b4eb5a35a95a59b91
peppercopia <peppercopia@gmail.com>
2016-09-30 22:03:53 -0400

    Corrected timer message.

--- init.lua
+++ init.lua
@@ -5,8 +5,9 @@
 -- Last Updated : 2016-09-30
 
 --timers used in application
---tmr(1) - Blink LED
+--tmr(1) - WiFi Connection Checker
 --tmr(2) - Slow blink LED
+--tmr(3) - Blink LED
 --tmr(6) - MQTT Poll Interval Publishing
 
 -- Load configuration and setup modules
